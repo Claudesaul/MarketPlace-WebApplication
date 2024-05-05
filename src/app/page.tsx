b6e3736f278c4b0eb7fb05fa9754fce976f06752
Claude Belizaire <139932571+Claudesaul@users.noreply.github.com>
2024-05-05 19:38:59 -0400

    2nd Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
       <MaxWidthWrapper>
     <div className={'py-20 mx-auto text-center flex flex-col items-center max-w-3xl'}>
-      <h1>Your Marketplace for the best digital <span className={'text-blue-600'}> digital assests</span>.
+      <h1>Your Marketplace for the best digital <span className={'text-blue-600'}> digital asses</span>.
       </h1>
     </div>
   </MaxWidthWrapper>
